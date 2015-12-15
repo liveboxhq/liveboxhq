440e9b01de1307ee435c9676becd7fced1bf243b
Willie <cheongwillie@gmail.com>
2015-12-15 02:04:51 -0500

    wip sales and marketing content

--- assets/main.js
+++ assets/main.js
@@ -57,10 +57,20 @@ jQuery(function($) {'use strict';
 		options = $.extend({}, options || {}, $this.data('countToOptions') || {});
 		$this.countTo(options);
 	}
-		
-	// Search
-	$('.fa-search').on('click', function() {
-		$('.field-toggle').fadeToggle(200);
+	
+	$(function() {
+	  	$('a[href*=#]:not([href=#])').click(function() {
+	    	if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+	      		var target = $(this.hash);
+	      		target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+	      		if (target.length) {
+			        $('html,body').animate({
+			          	scrollTop: target.offset().top
+			        }, 1000);
+			        return false;
+	      		}
+	    	}
+	  	});
 	});
 
 	// Contact form
